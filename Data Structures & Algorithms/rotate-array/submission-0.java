4c1d35e17ccc4d69380d0dfcb8a1c0ca8fd6e08d
Brittany Zhang <zhlcbri@icloud.com>
2026-07-03 15:17:45 -0700

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        // Step 0: Optimize k
+        int len = nums.length;
+        k = k % len;
+
+        // Step 1: Reverse the entire array
+        reverse(nums, 0, len - 1);
+        
+        // Step 2: Reverse the first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: Reverse the remaining elements
+        reverse(nums, k, len - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            // Swap numbers
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            // Move pointers towards the middle
+            start++;
+            end--;
+        }
+    }
+}
