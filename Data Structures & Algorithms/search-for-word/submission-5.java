f08cf9c2f0c0b43676a36e41965f8ad5f164ce49
Brittany Zhang <zhlcbri@icloud.com>
2026-07-03 16:41:50 -0700

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.java
@@ -0,0 +1,76 @@
+class Solution {
+    // Space: O(L); L = word.length()
+    // Time: O(row * col * 3^L)
+    // 除了第一步有四个方向，后面每一步只有三个方向，因为不能走回头路。
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+
+        // Scan the entire grid
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                // Skip this char           
+                if (board[r][c] != word.charAt(0)) {
+                    continue;
+                }
+                
+                // If the start char matches, activate search
+                if (search4D(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean search4D(
+        char[][] board, 
+        String word,
+        int r, 
+        int c, 
+        int index
+    ) {
+        // Success: found all chars in the word
+        // i.e. We can only arrive at word.length if we stepped on every char
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Failure 1: Out of grid boundaries
+        if (isOutOfBound(board, r, c)) {
+            return false;
+        }
+
+        // Failure 2: Char mismatch or already visited
+        if (board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+        // ===== Main searching steps =====
+        // 1. Save the char on current grid
+        char originalChar = board[r][c];
+
+        // 2. Mark the current grid as visited
+        board[r][c] = '*';
+
+        // 3. Search in 4 directions
+        boolean found = 
+            search4D(board, word, r + 1, c, index + 1) ||
+            search4D(board, word, r - 1, c, index + 1) ||
+            search4D(board, word, r, c + 1, index + 1) ||
+            search4D(board, word, r, c - 1, index + 1);
+
+        // 4. Erase the visited mark and restore the original char
+        board[r][c] = originalChar;
+
+        return found;
+    }
+
+    private boolean isOutOfBound(char[][] board, int r, int c) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
+            return true;
+        }
+        return false;
+    }
+}
